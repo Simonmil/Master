fb71178cd5cf20c9e3ab146bd9b6973179b1d2a9
Simon Millerjord <simonmillerjord@gmail.com>
2019-10-07 11:23:45 +0200

    Works

--- Code/GP_fix_bkg.py
+++ Code/GP_fix_bkg.py
@@ -24,12 +24,27 @@ def Bern(vars,pars):
 
 
 def neg_log_like(p):
-    ge.set_parameter_vector(p)
-    return -ge.log_likelihood(toy)
+    if len(p) > 2:
+        p[0] = ge.get_parameter_vector()[0]
+        p[1] = ge.get_parameter_vector()[1]
+        gesig.set_parameter_vector(p)
+        #print("sig:",p)
+        return -gesig.log_likelihood(toysig)
+    else:
+        ge.set_parameter_vector(p)
+        #print("bkg:",p)
+        return -ge.log_likelihood(toy)
 
 def grad_neg_log_like(p):
-    ge.set_parameter_vector(p)
-    return -ge.grad_log_likelihood(toy)
+    if len(p) > 2:
+        p[0] = ge.get_parameter_vector()[0]
+        p[1] = ge.get_parameter_vector()[1]
+        gesig.set_parameter_vector(p)
+        return -gesig.grad_log_likelihood(toysig)
+    else:
+        ge.set_parameter_vector(p)
+        return -ge.grad_log_likelihood(toy)
+
 
 
 tf = r.TFile.Open("diphox_shape_withGJJJDY_WithEffCor.root")
@@ -76,23 +91,51 @@ h_toy = h_truth.Clone("h_toy")
 h_toysig = h_truth.Clone("h_toysig")
 h_toy.Reset()
 h_toysig.Reset()
-
-
-for i_bin in range(1,h_truth.GetNbinsX()+1):
-    mass[i_bin-1] = h_truth.GetBinCenter(i_bin)
-    toy[i_bin-1] = R.Poisson(Bern5(mass[i_bin-1]))
-    toysig[i_bin-1] = R.Poisson(Bern5(mass[i_bin-1]) + signa(mass[i_bin-1]))
+Overfit = 0
+Underfit = 0
+for i in range(100):
+    for i_bin in range(1,h_truth.GetNbinsX()+1):
+        mass[i_bin-1] = h_truth.GetBinCenter(i_bin)
+        toy[i_bin-1] = R.Poisson(50*Bern5(mass[i_bin-1]))
+        toysig[i_bin-1] = toy[i_bin-1] + R.Poisson(50*signal(mass[i_bin-1]))
 
     kernel_bkg = np.median(toy)*george.kernels.ExpSquaredKernel(metric=np.exp(6))
-    ge = george.GP(kernel_bkg,solver=george.HODLRSolver)
+    ge = george.GP(kernel_bkg,solver=george.HODLRSolver,mean=np.median(toy))
     ge.compute(mass,yerr=np.sqrt(toy))
     m = minimize(neg_log_like,ge.get_parameter_vector(),jac=grad_neg_log_like)
     ge.set_parameter_vector(m.x)
     y_pred = ge.predict(toy,mass)[0]
 
-    kernel_sig = george.kernels.LocalGaussianKernel()
+    
+    chi2 = np.sum((toy-y_pred)**2/y_pred)
+    chi2ndf = chi2/(len(toy) - len(ge.get_parameter_vector()))
+    print("Chi2 background: ", chi2ndf)
+
+    kernel_sig = 40*george.kernels.LocalGaussianKernel(location=135,log_width=3)
     kernel_bkgsig = kernel_bkg + kernel_sig
-    ge = george.GP(kernel=kernel_bkgsig,solver=george.HODLRSolver)
-    m = minimize(neg_log_like,ge.get_parameter_vector(),jac=grad_neg_log_like)
-    ge.set_parameter_vector(m.x)
-    y_pred = ge.predict(toysig,mass)[0]
+    gesig = george.GP(kernel=kernel_bkgsig,solver=george.HODLRSolver)
+    gesig.compute(mass,yerr=np.sqrt(toysig))
+    m_sig = minimize(neg_log_like,gesig.get_parameter_vector(),jac=grad_neg_log_like,bounds=((20,25),(5,9),(1,100),(130,140),(1,4)))
+    gesig.set_parameter_vector(m_sig.x)
+
+    y_pred = gesig.predict(toysig,mass)[0]
+    #plt.scatter(mass,toysig,color='r',marker='.')
+    #plt.plot(mass,y_pred)
+    
+    chi2 = np.sum((toysig-y_pred)**2/y_pred)
+    #print(gesig.get_parameter_names())
+    print(chi2)
+    chi2ndf = chi2/(len(toysig) - len(gesig.get_parameter_vector()))
+    print("Chi2 signal + background: ", chi2ndf)
+    if chi2ndf<0.1:
+        Overfit += 1
+    if chi2ndf>10:
+        Underfit += 1    
+    print(gesig.get_parameter_vector())
+    plt.clf()
+    plt.scatter(mass,toysig,color='r',marker='.')
+    plt.plot(mass,y_pred)
+    plt.pause(0.05)
+
+print(Overfit)
+print(Underfit)
