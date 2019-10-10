69401a1fbe3ba2ae418ae61d75d2549b230c67fe
Simon Millerjord <simonmillerjord@gmail.com>
2019-10-10 14:58:16 +0200

    ALso has some interesting features

--- Code/GP_Fxd_prms_b4_toys.py
+++ Code/GP_Fxd_prms_b4_toys.py
@@ -48,7 +48,47 @@ def fit_minuit_gp(num,lnprob):
         init1 = np.random.random()*10.
         m = Minuit(lnprob,throw_nan=False,pedantic=False,print_level=0,Amp=init0,length=init1,
                     error_Amp = 10, error_length = 0.1,
-                    limit_Amp = (100,1e15), limit_length = (1,1000))
+                    limit_Amp = (100,1e15), limit_length = (1,20000))
+        
+        m.migrad()
+        if m.fval < minLLH:
+            minLLH = m.fval
+            best_fit_parameters = m.args
+    
+    print("min LL",minLLH)
+    print("best fit parameters",best_fit_parameters)
+    return minLLH, best_fit_parameters
+
+class log_like_gp_sig:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        
+    def __call__(self,Amp,length,Sigamp,sigma,mass):
+        kernel1 = Amp * george.kernels.ExpSquaredKernel(metric=length)
+        kernel2 = Sigamp*george.kernels.LocalGaussianKernel(location=mass,log_width=np.exp(sigma))
+        kernel = kernel1 + kernel2
+        gp = george.GP(kernel=kernel,solver=george.HODLRSolver)
+
+        try:
+            gp.compute(self.x,yerr=np.sqrt(self.y))
+            return -gp.log_likelihood(self.y)
+        except:
+            return np.inf
+
+def fit_minuit_gp_sig(num,lnprob):
+    minLLH = np.inf
+    best_fit_parameters = (0,0)
+    for i in range(num):
+        print(i+1)
+        init0 = np.random.random()*1e2
+        init1 = np.random.random()*10.
+        init2 = np.random.random()*10.
+        init3 = np.random.random()*1.
+        init4 = np.random.random()*10.
+        m = Minuit(lnprob,throw_nan=False,pedantic=False,print_level=0,Amp=init0,length=init1,Sigamp=init2,sigma=init3,mass=init4,
+                    error_Amp = 10, error_length = 0.1,error_Sigamp=np.sqrt(init2),error_sigma=0.01,error_mass=0.1,
+                    limit_Amp = (100,1e15), limit_length = (1,20000),limit_Sigamp = (0,400),limit_sigma=(0.5,4),limit_mass=(120,130))
         
         m.migrad()
         if m.fval < minLLH:
@@ -76,6 +116,7 @@ xmax = h_hist.GetXaxis().GetXmax()
 h_hist.Rebin(int(2./((xmax-xmin)/nbins)))
 
 
+
 Bern5 = r.TF1("Bern5",Bern,xmin,xmax,8)
 Bern5.SetParameters(1,0.1,0.01,0.001,0.0001,0.00001)
 Bern5.SetParNames("c0","c1","c2","c3","c4","c5","xmin","xmax")
@@ -87,13 +128,16 @@ binwidth = h_truth.GetBinWidth(1)
 nbins = h_truth.GetNbinsX()
 
 
+
 xmin = h_truth.GetXaxis().GetXmin()
 xmax = h_truth.GetXaxis().GetXmax()
-h_truth.Rebin(int(2./((xmax-xmin)/nbins)))
+#h_truth.Rebin(int(2./((xmax-xmin)/nbins)))
+#nbins = h_truth.GetNbinsX()
 
 
 
-Ntoys = 10
+
+Ntoys = 1
 mean = 125
 sigma = 2
 Amp = 200
@@ -156,7 +200,9 @@ chi2_fit = chi2/(len(toy)-len(gp.get_parameter_vector()))
 
 
 chi2_mean_gp = np.zeros(len(lum))
+chi2_mean_ge_err = np.zeros(len(lum))
 chi2_mean_par = np.zeros(len(lum))
+chi2_mean_par_err = np.zeros(len(lum))
 chi2_fit = np.zeros(Ntoys)
 h_chi2 = r.TH1D("h_chi2","Chi2 ad-hoc",100,0,20)
 color = ['r','b','g','c','m','k','chartreuse']
@@ -231,8 +277,8 @@ plt.title("Test statistic distribution")
 
 plt.figure(2)
 
-plt.plot(lum,chi2_mean_gp,color='r', label='GP')
-plt.plot(lum,chi2_mean_par,color='b',label='Ad-hoc')
+plt.errorbar(lum,chi2_mean_gp,color='r', label='GP')
+plt.errorbar(lum,chi2_mean_par,color='b',label='Ad-hoc')
 plt.xlabel('Luminosity scale factor')
 plt.ylabel(r'$\chi^2/ndf$')
 plt.legend()
@@ -244,7 +290,52 @@ plt.show()
 """ Spurious signal testing"""
 
 
+Ntoys = 10
+for t in range(Ntoys):
+    for i_bin in range(1,h_truth.GetNbinsX()+1):
+        toy[i_bin-1] = R.Poisson(50*(Bern5(mass[i_bin-1])+signal(mass[i_bin-1])))
+        h_toy.SetBinContent(i_bin,toy[i_bin-1])
+        h_toy.SetBinError(i_bin,np.sqrt(toy[i_bin-1]))
+    mass_blind = []
+    toy_blind = []
+    for i in range(len(mass)):
+        if mass[i] < 120 or mass[i] > 130:
+            mass_blind.append(mass[i])
+            toy_blind.append(toy[i])
+    mass_blind = np.array(mass_blind)
+    toy_blind = np.array(toy_blind)
+        
+    lnprob = log_like_gp(mass_blind,toy_blind)
+    minLLH, best_fit_parameters = fit_minuit_gp(100,lnprob)
+    kernel = best_fit_parameters[0]*george.kernels.ExpSquaredKernel(metric=best_fit_parameters[1])
+    #kernel = np.exp(100)*george.kernels.ExpSquaredKernel(metric=best_fit_parameters[1])
+    gp = george.GP(kernel,solver=george.HODLRSolver)
+    gp.compute(mass_blind,yerr=np.sqrt(toy_blind))
 
+    y_pred, y_var = gp.predict(toy_blind,mass,return_var=True)
+    
+    lnprob_sig = log_like_gp_sig(mass,toy)
+    minLLH_sig,best_fit_parameters_sig = fit_minuit_gp_sig(100,lnprob_sig)
+    kernel1 = best_fit_parameters[0] * george.kernels.ExpSquaredKernel(metric=best_fit_parameters_sig[1])
+    kernel2 = best_fit_parameters_sig[2]*george.kernels.LocalGaussianKernel(location=best_fit_parameters_sig[4],log_width=best_fit_parameters_sig[3]**2)
+    kernel = kernel1 + kernel2
+    gp = george.GP(kernel=kernel,solver=george.HODLRSolver)
+    gp.compute(mass,yerr=np.sqrt(toy))
+
+
+    y_pred_sig, y_var_sig = gp.predict(toy,mass,return_var=True)
+
+    res_pred = y_pred_sig-y_pred
+    res_Std = np.sqrt(y_var + y_var_sig)
+
+    plt.clf()
+    plt.scatter(mass,toy,c='r',alpha=0.8,marker='.')
+    plt.plot(mass,y_pred,'b-')
+    plt.plot(mass,y_pred_sig,'g-')
+    plt.plot(mass,res_pred,'k-')
+    plt.fill_between(mass,y_pred-np.sqrt(y_var),y_pred+np.sqrt(y_var),color='k',alpha=0.4)
+    plt.fill_between(mass,res_pred-res_Std,res_pred+res_Std,color='k',alpha=0.4)
+    plt.pause(0.05)
 
 
 
